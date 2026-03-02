f5280fac84e6e25d85ea745fea242d13d75e6dd6
CagesThrottleUs <manstein.felix@gmail.com>
2026-03-02 09:53:52 +0530

    feat(codeforces): add solution for 1853A
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

--- solutions/1853A.cpp
+++ solutions/1853A.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+
+#ifdef __GNUC__
+#ifndef __clang__
+#pragma GCC optimize("O3", "unroll-loops")
+#endif
+#endif
+
+void speedup() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+}
+
+// Forward declaration - to be implemented in problem file
+void solve();
+
+auto main() -> int {
+    speedup();
+    solve();
+    return 0;
+}
+
+// Include the problem-specific implementation
+
+#include <iostream>
 #include <limits>
 
 auto work() -> int {
