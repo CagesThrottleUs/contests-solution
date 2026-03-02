b97a2e41a5f622b4051000a0fbc2cb1fe5f85120
CagesThrottleUs <manstein.felix@gmail.com>
2026-03-02 10:17:26 +0530

    feat(codeforces): add solution for 1845A
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ solutions/1845A.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+#ifdef __GNUC__
+#ifndef __clang__
+#pragma GCC optimize("O3", "unroll-loops")
+#endif
+#endif
+
+void speedup() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+}
+
+// Forward declaration - to be implemented in problem file
+void solve();
+
+auto main() -> int {
+    speedup();
+    solve();
+    return 0;
+}
+
+// Include the problem-specific implementation
+
+#include <iostream>
+
+void print(int n, int times) {
+  if (times == 0) return;
+  std::cout << n;
+  for (int i{0}; i < times - 1; i++) {
+    std::cout << " " << n;
+  }
+}
+
+void work() {
+  int n{}, k{}, x{};
+  std::cin >> n >> k >> x;
+
+  if ((k == 1 && x == 1) || (k == 2 && x == 1 && (n & 1))) {
+    std::cout << "NO\n";
+    return;
+  }
+
+  std::cout << "YES\n";
+  if (x == 1) {
+    int c2 = (n / 2) - (n & 1), c3 = (n & 1);
+    std::cout << c2 + c3 << "\n";
+    print(2, c2);
+    std::cout << " ";
+    print(3, c3);
+  } else {
+    std::cout << n << "\n";
+    print(1, n);
+  }
+  std::cout << "\n";
+}
+
+void solve() {
+  int tests{};
+  std::cin >> tests;
+
+  while (tests--) {
+    work();
+  }
+}
