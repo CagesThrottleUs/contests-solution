1c9f0c8baaed8e1b81c94596afb88756cd3000e9
CagesThrottleUs <manstein.felix@gmail.com>
2026-03-08 16:14:28 +0530

    feat(codeforces): add 1869A
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ codeforces/1869A.cpp
@@ -0,0 +1,54 @@
+/*
+ *   MIT License
+ *
+ *   Copyright (c) 2026 CagesThrottleUs
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <iostream>
+
+void solve() {
+  int tests{};
+  std::cin >> tests;
+
+  while ((tests--) != 0) {
+    int ele_cnt{};
+    std::cin >> ele_cnt;
+
+    int xor_res{0};
+    for (int i{0}; i < ele_cnt; i++) {
+      int input{};
+      std::cin >> input;
+      xor_res = xor_res xor input;  // NOLINT(hicpp-signed-bitwise)
+    }
+
+    if (xor_res == 0) {
+      std::cout << "1\n1 " << ele_cnt << "\n";
+    } else {
+      if ((ele_cnt & 1) == 0) {  // NOLINT(hicpp-signed-bitwise)
+        std::cout << "2\n1 " << ele_cnt << "\n1 " << ele_cnt << "\n";
+      } else {
+        std::cout << "4\n1 " << ele_cnt << "\n1 " << ele_cnt - 1 << "\n"
+                  << ele_cnt - 1 << " " << ele_cnt << "\n"
+                  << ele_cnt - 1 << " " << ele_cnt << "\n";
+      }
+    }
+  }
+}
