a8459e6d04b5ec3e0eab9c90525f999d3d2ca94f
CagesThrottleUs <manstein.felix@gmail.com>
2026-03-09 06:42:48 +0530

    feat(codeforces): add 1837B
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ solutions/1837B.cpp
@@ -0,0 +1,88 @@
+/*
+ *   MIT License
+ *
+ *   Copyright (c) 2026 CagesThrottleUs
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ */
+
+#include <iostream>
+
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC optimize("O3", "unroll-loops")
+#elif defined(__clang__)
+#pragma clang optimize on
+#endif
+
+void speedup() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+}
+
+// Forward declaration - to be implemented in problem file
+void solve();
+
+auto main() -> int {
+    speedup();
+    solve();
+    return 0;
+}
+
+// Include the problem-specific implementation
+
+#include <algorithm>
+#include <cstddef>
+#include <iostream>
+#include <string>
+
+auto find_range_len(const std::string& str) -> int {
+  int longest_seq{0};
+  char last_char{0};
+  int curr_seq{0};
+
+  for (const auto& _ch : str) {
+    if (_ch == last_char) {
+      curr_seq++;
+    } else {
+      last_char = _ch;
+      curr_seq = 1;
+    }
+    longest_seq = std::max(longest_seq, curr_seq);
+  }
+
+  return longest_seq + 1;
+}
+
+void solve() {
+  int tests{};
+  std::cin >> tests;
+
+  while ((tests--) != 0) {
+    int size{};
+    std::cin >> size;
+
+    std::string str;
+    str.reserve(static_cast<std::size_t>(size));
+    std::cin >> str;
+
+    auto range_len = find_range_len(str);
+    std::cout << range_len << "\n";
+  }
+}
