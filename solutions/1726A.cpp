005e5b0bbfc712d295cd2537fb85a3310cc7866a
CagesThrottleUs <manstein.felix@gmail.com>
2026-03-10 06:40:18 +0530

    feat(codeforces): add 1726A
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ solutions/1726A.cpp
@@ -0,0 +1,87 @@
+/*
+ *   MIT License
+ *
+ *   Copyright (c) 2026 CagesThrottleUs
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ */
+
+#include <iostream>
+
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC optimize("O3", "unroll-loops")
+#elif defined(__clang__)
+#pragma clang optimize on
+#endif
+
+void speedup() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+}
+
+// Forward declaration - to be implemented in problem file
+void solve();
+
+auto main() -> int {
+    speedup();
+    solve();
+    return 0;
+}
+
+// Include the problem-specific implementation
+
+#include <algorithm>
+#include <iostream>
+#include <limits>
+void solve() {
+  int tests{};
+  std::cin >> tests;
+
+  while ((tests--) != 0) {
+    int size{};
+    std::cin >> size;
+
+    int max_ele{std::numeric_limits<int>::min()};
+    int min_ele{std::numeric_limits<int>::max()};
+
+    int max_diff{std::numeric_limits<int>::min()};
+
+    int first{};
+    int last{};
+
+    for (int i{0}; i < size; i++) {
+      int ele{};
+      std::cin >> ele;
+      max_ele = std::max(max_ele, ele);
+      min_ele = std::min(min_ele, ele);
+      if (i == 0) {
+        first = ele;
+        last = ele;
+        continue;
+      }
+      max_diff = std::max(max_diff, last - ele);
+      last = ele;
+    }
+
+    auto ans =
+        std::max({last - first, max_diff, last - min_ele, max_ele - first});
+    std::cout << ans << "\n";
+  }
+}
