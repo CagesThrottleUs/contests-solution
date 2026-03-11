c04bc9a2f8bdeeb18c920b8958398ecb18d6a3aa
CagesThrottleUs <manstein.felix@gmail.com>
2026-03-11 07:28:44 +0530

    feat(codeforces): add 1666D
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ codeforces/1666D.cpp
@@ -0,0 +1,68 @@
+/*
+ *   MIT License
+ *
+ *   Copyright (c) 2026 CagesThrottleUs
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <stack>
+#include <string>
+#include <unordered_map>
+
+void solve() {
+  int tests{};
+  std::cin >> tests;
+
+  while ((tests--) != 0) {
+    std::unordered_map<char, std::stack<std::size_t>> char_idx_map;
+
+    std::string temp;
+    std::cin >> temp;
+
+    for (std::size_t i{0}; i < temp.length(); i++) {
+      char_idx_map[temp[i]].push(i);
+    }
+
+    std::cin >> temp;
+    bool impossible{false};
+    std::size_t last{std::numeric_limits<std::size_t>::max()};
+    for (std::size_t i{temp.length() - 1}; i < temp.length(); i--) {
+      if (!char_idx_map.contains(temp[i]) || char_idx_map[temp[i]].empty()) {
+        impossible = true;
+        break;
+      }
+      if (char_idx_map[temp[i]].top() > last) {
+        impossible = true;
+        break;
+      }
+      last = char_idx_map[temp[i]].top();
+      char_idx_map[temp[i]].pop();
+    }
+
+    if (impossible) {
+      std::cout << "NO\n";
+    } else {
+      std::cout << "YES\n";
+    }
+  }
+}
