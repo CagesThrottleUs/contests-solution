d5e64a1c6b169054b0de1d7c1c48505321c5ec42
CagesThrottleUs <manstein.felix@gmail.com>
2026-03-12 08:18:48 +0530

    feat(codeforces): add 1624B
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ codeforces/1624B.cpp
@@ -0,0 +1,59 @@
+/*
+ *   MIT License
+ *
+ *   Copyright (c) 2026 CagesThrottleUs
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <iostream>
+
+void solve() {
+  int tests{};
+  std::cin >> tests;
+
+  while ((tests--) != 0) {
+    int first{};
+    int second{};
+    int third{};
+    std::cin >> first >> second >> third;
+
+    auto lhs = second - first;
+    auto rhs = third - second;
+
+    bool is_possible = false;
+    if (lhs == rhs) {
+      is_possible = true;
+    } else if (lhs > rhs) {
+      auto num = (2 * second) - first;
+      is_possible = (num > 0) and (num % third == 0);
+      num = -(third - (2 * second));
+      is_possible = is_possible or ((num > 0) and (num % first == 0));
+    } else {
+      auto num = (third + first);
+      is_possible = (num > 0) and (num % (2 * second) == 0);
+    }
+
+    if (is_possible) {
+      std::cout << "YES\n";
+    } else {
+      std::cout << "NO\n";
+    }
+  }
+}
