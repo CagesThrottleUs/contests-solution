abc64f15742cee748fec55795b907cdc492f14c4
CagesThrottleUs <manstein.felix@gmail.com>
2026-03-16 05:36:49 +0530

    feat(codeforces): add 1582B
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ codeforces/1582B.cpp
@@ -0,0 +1,49 @@
+/*
+ *   MIT License
+ *
+ *   Copyright (c) 2026 CagesThrottleUs
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <iostream>
+void solve() {
+  int tests{};
+  std::cin >> tests;
+
+  while ((tests--) != 0) {
+    std::size_t cnt_ones{0};
+    std::size_t cnt_zeros{0};
+
+    int loop_size{0};
+    std::cin >> loop_size;
+
+    for (int i{0}; i < loop_size; i++) {
+      int num{};
+      std::cin >> num;
+
+      cnt_ones += static_cast<std::size_t>(num == 1);
+      cnt_zeros += static_cast<std::size_t>(num == 0);
+    }
+
+    auto ans = (1ULL << cnt_zeros);
+    ans *= cnt_ones;
+    std::cout << ans << "\n";
+  }
+}
