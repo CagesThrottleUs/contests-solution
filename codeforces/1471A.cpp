7314d20e5e15364318b2b12c8fb1c2c40b86e0fe
CagesThrottleUs <manstein.felix@gmail.com>
2026-03-17 06:38:27 +0530

    feat(codeforces): add 1471A
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ codeforces/1471A.cpp
@@ -0,0 +1,54 @@
+/*
+ *   MIT License
+ *
+ *   Copyright (c) 2026 CagesThrottleUs
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
+void solve() {
+  int tests{};
+  std::cin >> tests;
+
+  while ((tests--) != 0) {
+    std::size_t arr_len{};
+    int divisor{};
+    std::cin >> arr_len >> divisor;
+
+    std::int64_t sum{0};
+    std::int64_t max_beauty{0};
+
+    for (std::size_t i{0}; i < arr_len; i++) {
+      int num{};
+      std::cin >> num;
+      sum += num;
+
+      max_beauty +=
+          (num / divisor) + static_cast<std::int64_t>((num % divisor) != 0);
+    }
+
+    auto min_beauty =
+        (sum / divisor) + static_cast<std::int64_t>((sum % divisor) != 0);
+
+    std::cout << min_beauty << " " << max_beauty << "\n";
+  }
+}
