e9492db1922db2eca5806c1ce92684288ac3b713
CagesThrottleUs <manstein.felix@gmail.com>
2026-03-18 20:13:15 +0530

    feat(codeforces): add 1913B
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ solutions/1913B.cpp
@@ -0,0 +1,87 @@
+/*
+ *   MIT License
+ *
+ *   Copyright (c) 2026 CagesThrottleUs
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <iostream>
+
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC optimize("O3", "unroll-loops")
+#elif defined(__clang__)
+#pragma clang optimize on
+#endif
+
+void speedup() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+}
+
+// Forward declaration - to be implemented in problem file
+void solve();
+
+auto main() -> int {
+  speedup();
+  solve();
+  return 0;
+}
+
+// Include the problem-specific implementation
+
+#include <iostream>
+#include <string>
+
+void solve() {
+  int tests{};
+  std::cin >> tests;
+
+  while ((tests--) != 0) {
+    std::string str{};
+    std::cin >> str;
+
+    int ones{0};
+    int zeros{0};
+    for (auto &item : str) {
+      if (item == '1') {
+        ones++;
+      } else {
+        zeros++;
+      }
+    }
+
+    int len_t{0};
+
+    for (std::size_t i{0}; i < str.size(); i++) {
+      if (str[i] == '1' and zeros > 0) {
+        len_t++;
+        zeros--;
+      } else if (str[i] == '0' and ones > 0) {
+        len_t++;
+        ones--;
+      } else {
+        break;
+      }
+    }
+    
+    std::cout << static_cast<int>(str.size()) - len_t << "\n";
+  }
+}
