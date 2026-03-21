2f88b8096a93ef428058967480cee96ea2a98359
CagesThrottleUs <manstein.felix@gmail.com>
2026-03-21 07:53:33 +0530

    feat(codeforces): add 1883C
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ codeforces/1883C.cpp
@@ -0,0 +1,63 @@
+/*
+ *   MIT License
+ *
+ *   Copyright (c) 2026 CagesThrottleUs
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <limits>
+
+void solve() {
+  int tests{};
+  std::cin >> tests;
+
+  while ((tests--) != 0) {
+    int arr_size{};
+    int divisor{};
+    std::cin >> arr_size >> divisor;
+
+    int min_ops{std::numeric_limits<int>::max()};
+
+    int total_power_2{0};
+
+    for (int i = 0; i < arr_size; i++) {
+      int num{};
+      std::cin >> num;
+      int rem = num % divisor;
+      while (num % 2 == 0) {
+        num /= 2;
+        total_power_2++;
+      }
+      min_ops = std::min(min_ops, rem == 0 ? 0 : divisor - rem);
+    }
+
+    if (divisor == 4) {
+      if (total_power_2 >= 2) {
+        min_ops = 0;
+      } else {
+        min_ops = std::min(min_ops, 2 - total_power_2);
+      }
+    }
+
+    std::cout << min_ops << "\n";
+  }
+}
