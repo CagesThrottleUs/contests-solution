05d55410515fccd21ce235d1b914fc51bf86b13b
CagesThrottleUs <manstein.felix@gmail.com>
2026-03-21 10:47:35 +0530

    feat(codeforces): add 1840C
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ solutions/1840C.cpp
@@ -0,0 +1,83 @@
+/*
+ *   MIT License
+ *
+ *   Copyright (c) 2026 CagesThrottleUs
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <iostream>
+
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC optimize("O3", "unroll-loops")
+#elif defined(__clang__)
+#pragma clang optimize on
+#endif
+
+void speedup() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+}
+
+// Forward declaration - to be implemented in problem file
+void solve();
+
+auto main() -> int {
+  speedup();
+  solve();
+  return 0;
+}
+
+// Include the problem-specific implementation
+
+#include <cstddef>
+#include <iostream>
+
+void solve() {
+  int tests{};
+  std::cin >> tests;
+
+  while ((tests--) != 0) {
+    int arr_size{};
+    std::cin >> arr_size;
+    int sub_arr_min_size{};
+    std::cin >> sub_arr_min_size;
+    int max_temp_allowed{};
+    std::cin >> max_temp_allowed;
+
+    std::size_t cnt{0};
+    int last_bad{-1};
+
+    for (int i{0}; i < arr_size; i++) {
+      int temp{};
+      std::cin >> temp;
+      if (temp > max_temp_allowed) {
+        last_bad = i;
+        continue;
+      }
+      if (i - last_bad < sub_arr_min_size) {
+        continue;
+      }
+      cnt += static_cast<std::size_t>(i - last_bad - sub_arr_min_size + 1);
+    }
+
+    std::cout << cnt << "\n";
+  }
+}
