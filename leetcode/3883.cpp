7e474e7c5d1c53ce8a8d6401c75366fbc193088f
CagesThrottleUs <manstein.felix@gmail.com>
2026-03-29 07:31:30 +0530

    style(leetcode): align 3883. Count Non Decreasing Arrays With Given Digit Sums
    Done similar to Google Style Guide
    
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

--- leetcode/3883.cpp
+++ leetcode/3883.cpp
@@ -43,26 +43,26 @@ class Solution {
   }
 
  public:
-  static auto countArrays(std::vector<int>& digitSum) -> int {
+  static auto countArrays(std::vector<int>& digit_sum) -> int {
     // Precompute candidates for each digit sum value in [0, kMaxVal]
-    std::array<std::vector<int>, MAX_DIGIT_SUM + 1> bySum{};
+    std::array<std::vector<int>, MAX_DIGIT_SUM + 1> by_sum{};
     for (int val = 0; val <= MAX_VAL; ++val) {
       int sum = computeDigitSum(val);
       if (sum <= MAX_DIGIT_SUM) {
-        bySum.at(static_cast<std::size_t>(sum)).push_back(val);
+        by_sum.at(static_cast<std::size_t>(sum)).push_back(val);
       }
     }
 
-    const auto len = digitSum.size();
+    const auto len = digit_sum.size();
 
     // cands[i] = sorted list of valid values for position i
     std::vector<std::vector<int>> cands(len);
     for (std::size_t idx = 0; idx < len; ++idx) {
-      int dsum = digitSum.at(idx);
+      int dsum = digit_sum.at(idx);
       if (dsum < 0 || dsum > MAX_DIGIT_SUM) {
         return 0;
       }
-      cands.at(idx) = bySum.at(static_cast<std::size_t>(dsum));
+      cands.at(idx) = by_sum.at(static_cast<std::size_t>(dsum));
       if (cands.at(idx).empty()) {
         return 0;
       }
@@ -86,7 +86,7 @@ class Solution {
       std::vector<std::int64_t> newWays(curr.size(), 0);
       for (std::size_t kdx = 0; kdx < curr.size(); ++kdx) {
         // Count prev candidates <= curr[kdx] via binary search (prev is sorted)
-        auto iter = std::upper_bound(prev.begin(), prev.end(), curr.at(kdx));
+        auto iter = std::ranges::upper_bound(prev, curr.at(kdx));
         auto cnt = static_cast<std::size_t>(iter - prev.begin());
         newWays.at(kdx) = prefix.at(cnt);
       }
@@ -95,8 +95,8 @@ class Solution {
     }
 
     std::int64_t ans = 0;
-    for (auto wval : ways) {
-      ans = (ans + wval) % MOD;
+    for (auto way : ways) {
+      ans = (ans + way) % MOD;
     }
     return static_cast<int>(ans);
   }
