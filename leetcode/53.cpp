153a3098ca0e21e4fe28e46c41b96cc2f54a385a
CagesThrottleUs <manstein.felix@gmail.com>
2026-06-20 18:38:02 +0530

    refactor(leetcode): add 53. Maximum Subarray
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

--- leetcode/53.cpp
+++ leetcode/53.cpp
@@ -1,37 +1,14 @@
-/*
- *   MIT License
- *
- *   Copyright (c) 2026 CagesThrottleUs
- *
- *   Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- *   furnished to do so, subject to the following conditions:
- *
- *   The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- */
-
 #include <algorithm>
+#include <cstdint>
 #include <limits>
 #include <vector>
 
 class Solution {
  public:
-  auto maxSubArray(std::vector<int>& nums) -> int {
-    int max_sum{std::numeric_limits<int>::min()};
-    int sum{0};
-    
+  static auto maxSubArray(std::vector<std::int32_t>& nums) -> std::int32_t {
+    std::int32_t max_sum{std::numeric_limits<std::int32_t>::min()};
+    std::int32_t sum{0};
+
     for (const auto& num : nums) {
       sum += num;
       max_sum = std::max(max_sum, sum);
